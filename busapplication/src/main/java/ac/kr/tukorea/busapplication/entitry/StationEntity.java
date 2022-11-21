9fbe402182f36960b6a240228acf8bc261c833a1
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2022-11-22 02:29:43 +0900

    station table's Entity

---
+++ busapplication/src/main/java/ac/kr/tukorea/busapplication/entitry/StationEntity.java
@@ -0,0 +1,45 @@
+package ac.kr.tukorea.busapplication.entitry;
+
+import javax.persistence.*;
+
+@Entity(name = "Station")
+@Table(name="station_table")
+public class StationEntity {
+    @Id
+    @Column(
+            name = "id"
+    )
+    private String id;
+
+    @Column(
+            name = "name",
+            nullable = false
+    )
+    private String name;
+
+    @Column(
+            name = "city_name",
+            nullable = false
+    )
+    private String city_name;
+
+    @Column(
+            name = "number",
+            nullable = false
+    )
+    private int number;
+
+    @Column(
+            name = "gps_x",
+            nullable = false
+    )
+    private double gps_x;
+
+    @Column(
+            name = "gps_y",
+            nullable = false
+    )
+    private double gps_y;
+
+
+}
