fc30cf3ceabf795f7df11ac618fc029a9a47def3
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2022-11-21 19:17:27 +0900

    commit

---
+++ busapplication/src/test/java/ac/kr/tukorea/busapplication/BusapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.kr.tukorea.busapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
