f65a923a3be34833bf902f442683818c872182af
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2022-11-22 17:04:43 +0900

    routestation 테이블 Entity

---
+++ busapplication/src/main/java/ac/kr/tukorea/busapplication/entitry/RouteStationEntity.java
@@ -0,0 +1,33 @@
+package ac.kr.tukorea.busapplication.entitry;
+
+import javax.persistence.*;
+
+@Entity(name = "RouteStation")
+@Table(name = "route_station_table")
+public class RouteStationEntity {
+    @Id
+    @Column(
+            name = "id"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY
+    )
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "route_id")
+    private RouteEntity route;
+
+    @ManyToOne
+    @JoinColumn(name = "station_id")
+    private StationEntity station;
+
+    @Column(
+            name = "order",
+            nullable = false
+    )
+    private int order;
+
+
+
+}
