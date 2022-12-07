3ce1980a60a7c53d35d2edef8bc59dbdb736efe3
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2022-12-07 18:59:00 +0900

    검색 기능 모두 구현, 버스 기사 해야함

--- busapplication/src/main/java/ac/kr/tukorea/busapplication/Repository/BusRepository.java
+++ busapplication/src/main/java/ac/kr/tukorea/busapplication/Repository/BusRepository.java
@@ -3,10 +3,10 @@ package ac.kr.tukorea.busapplication.repository;
 
 import ac.kr.tukorea.busapplication.DTO.BusDTO;
 import ac.kr.tukorea.busapplication.entity.BusEntity;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 @Repository
@@ -14,9 +14,10 @@ public interface BusRepository extends JpaRepository<BusEntity, Long> {
     @Query(value = "select * from bus_table b inner join route_table r on b.route_id = r.id where r.id = :routeid", nativeQuery = true)
     List<BusDTO> findAllByRoute(int routeid);
 
-    @Query(value = "select * from bus_table b where b.route_id = :routeid and b.bus_id = :busid", nativeQuery = true)
+    @Query(value = "select * from bus_table b where b.route_id = :routeid and b.bus_id = :busid limit 1", nativeQuery = true)
     BusDTO findByRoute_idAndBus_id(int routeid, String busid);
 
-    @Query(value = "select * from bus_table b where b.route_id = :routeid and b.current_stop < :stop_order",nativeQuery = true)
-    BusDTO findByRoute_idAndAndCurrent_stop(int routeid, int stop_order);
+    @Query(value = "select new ac.kr.tukorea.busapplication.DTO.BusDTO(b.id, b.route_id, b.bus_id, b.init_order, b.gps_x, b.gps_y, b.current_stop)" +
+            " from Bus b where b.route_id = :routeid and b.current_stop < :stop_order order by b.current_stop desc")
+    List<BusDTO> findTop1ByRoute_idAndAndCurrent_stop(int routeid, int stop_order);
 }
